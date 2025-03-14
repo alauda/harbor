a1f358b839608229f2a0a77100ef4638379db7a0
chengjingtao <jtcheng0616@gmail.com>
2025-03-14 17:23:23 +0800

    fix beego vuln and update docker, helm/v3 package version, and fix chartm vuln
    Signed-off-by: chengjingtao <jtcheng0616@gmail.com>

--- make/patches/patch-arm64.sh
+++ make/patches/patch-arm64.sh
@@ -39,6 +39,10 @@ sed -i 's/CGO_ENABLED=0/GOOS=linux GOARCH=arm64 CGO_ENABLED=0/g' "make/photon/tr
 echo "ARM64 after change make/photon/trivy-adapter/Dockerfile.binary is "
 cat make/photon/trivy-adapter/Dockerfile.binary
 
+sed -i 's/CGO_ENABLED=0/GOOS=linux GOARCH=arm64 CGO_ENABLED=0/g' "make/photon/trivy-adapter/Dockerfile.trivy.binary"
+echo "ARM64 after change make/photon/trivy-adapter/Dockerfile.trivy.binary is "
+cat make/photon/trivy-adapter/Dockerfile.trivy.binary
+
 sed -i '8 a ENV CGO_ENABLED 0 \nENV GOOS linux \nENV GOARCH arm64' "make/photon/notary/binary.Dockerfile"
 sed -i 's/\/go\/bin\/cli/\/go\/bin\/linux_arm64\/cli/g' "make/photon/notary/binary.Dockerfile"
 
