14c4dd37fa5659ce71bbae5662ae15741a38ac8c
yuzichen <1062955096@qq.com>
2025-05-21 10:58:29 +0800

    fix: fix vuln for devops v3 harbor

--- make/patches/patch-amd64.sh
+++ make/patches/patch-amd64.sh
@@ -9,7 +9,7 @@ change_base_image () {
             change_base_image $1"/"$file
         elif [[ $file == *Dockerfile* ]]
         then
-            sed -i 's/photon:4.0/build-harbor.alauda.cn\/ops\/photon:4-alauda-202503111116/' $1"/"$file
+            sed -i 's/photon:4.0/build-harbor.alauda.cn\/ops\/photon:4-alauda-202505080514/' $1"/"$file
             sed -i 's/node:16.10.0/build-harbor.alauda.cn\/devops\/node:16.10.0/' $1"/"$file
             
             echo $1"/"$file
