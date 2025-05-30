331dbfeecaade8f384e0a070dbd94f91b4eae0bf
yuzichen <1062955096@qq.com>
2025-05-30 17:49:02 +0800

    [DEVOPS-40524] fix: fix vuln for devops v4 harbor, add debug log temporary

--- src/server/middleware/csrf/csrf.go
+++ src/server/middleware/csrf/csrf.go
@@ -79,9 +79,11 @@ func Middleware() func(handler http.Handler) http.Handler {
 			csrf.Path("/"))
 	})
 	return middleware.New(func(rw http.ResponseWriter, req *http.Request, next http.Handler) {
+		log.Infof("***** secureFlag: %t", secureFlag)
 		if !secureFlag {
 			req = csrf.PlaintextHTTPRequest(req)
 		}
+		log.Infof("***** req value: %v", req.Context().Value(csrf.PlaintextHTTPContextKey))
 		protect(attach(next)).ServeHTTP(rw, req)
 	}, csrfSkipper)
 }
@@ -103,5 +105,7 @@ func secureCookie() bool {
 		log.Warningf("Failed to get external endpoint: %v, set cookie secure flag to true", err)
 		return true
 	}
+	log.Infof("***** External endpoint: %s", ep)
+	log.Infof("***** External endpoint has prefix: %t", !strings.HasPrefix(strings.ToLower(ep), "http://"))
 	return !strings.HasPrefix(strings.ToLower(ep), "http://")
 }
